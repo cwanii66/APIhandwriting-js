182636d39cad68fe58eb226fdbc22ae018949804
Chris Wong <wh18710455626@126.com>
2021-10-19 21:09:38 +0800

    vstest

---
+++ new-operator.js
@@ -0,0 +1,24 @@
+// new create a total new Object
+// new link to __proto__
+// this -> new Object
+// Object -> function prototype
+//
+
+function New(func) {
+	let res = {};
+	if (func.prototype !== null) {
+		res.__proto__ = func.prototype;
+	}
+
+	let ret = func.apply(res, Array.prototype.slice.call(arguments, 1)); //Array.prototype.slice.call ===> Array.from(arguments)   then data handling
+	if ((typeof ret === 'Object' || typeof ret === 'function') && ret != null) {
+		return ret;
+	}
+	return res;
+}
+
+let obj = new(A, 1, 2);
+//equals to 
+
+let obj = new A(1, 2);
+
