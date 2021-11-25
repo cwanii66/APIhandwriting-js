fe681e4047476a702e559192aa8f3451982bb791
Chris Wong <wh18710455626@126.com>
2021-11-25 10:48:41 +0800

    javascript strategy

--- strategy/form.js
+++ strategy/form.js
@@ -12,7 +12,7 @@
  *  手机号码必须符合格式。
  */
 
-let strategies = {
+let strategies = {  // 单一职责
     isNoneEmpty(value, errorMsg) {
         if (value === '') {
             return errorMsg;
@@ -83,3 +83,129 @@ class Validator {
 }
 
 // 5.6.3文本输入框添加多种校验规则
+// 期望
+validator.add(registerForm.userName, [{
+    strategy: 'isNonEmpty',
+    errorMsg: '用户名不能为空'
+}, {
+    strategy: 'minLength',
+    errorMsg: '用户名长度不能小于10位'
+}]);
+
+/**************策略对象*****************/
+
+let strategies_ = {
+    isNonEmpty(value, errorMsg) {
+        if (value === '') {
+            return errorMsg;
+        }
+    },
+
+    minLength(value, length, errorMsg) {
+        if (value.length < length) {
+            return errorMsg;
+        }
+    },
+
+    isMobile(value, errorMsg) {
+        if (!/(^1[3|5|8][0-9]{9}$)/.test(value)) {
+            return errorMsg;
+        }
+    }
+};
+
+/** *********Validator类*********** */
+class Validator_ {  // validator class 实际上是用户和处理用户行为的中间件
+    constructor() {
+        this.cache = [];
+    }
+
+    add(dom, rules) { // dom 取得校验结点和调用规则
+        for (let i = 0, rule; rule = rules[i++]; ) {
+            let strategyAry = rule.strategy.split(':');
+            let errorMsg = rule.errorMsg;
+            this.cache.push(function() {
+                let strategy = strategyAry.shift();
+                strategyAry.unshift(dom.value);
+                strategyAry.push(errorMsg);
+                return strategies_[strategy].apply(dom, strategyAry);
+            })
+        }
+    }
+
+    start() {
+        for (let i = 0, validatorFunc; validatorFunc = this.cache[i++]; ) {
+            let errorMsg = validatorFunc();
+            if (errorMsg) {
+                return errorMsg;
+            }
+        }
+    }
+}
+
+
+/* ***************用户调用*****************/
+
+let registerForm_ = document.getElementById('registerForm');
+
+let validateFunc_ = function() {
+    let validator = new Validator_();
+    validator.add( registerForm.userName, [{
+        strategy: 'isNonEmpty',
+        errorMsg: '用户名不能为空'
+    }, {
+        strategy: 'minLength:6',
+        errorMsg: '用户名长度不能小于 10 位'
+    }]);
+    validator.add( registerForm.password, [{
+        strategy: 'minLength:6',
+        errorMsg: '密码长度不能小于 6 位'
+    }]);
+    validator.add( registerForm.phoneNumber, [{
+        strategy: 'isMobile',
+        errorMsg: '手机号码格式不正确'
+    }]);
+
+    let errorMsg_ = validator.start();
+    return errorMsg_;
+}
+
+registerForm_.onsubmit = function() {
+    let errorMsg = validateFunc_();
+    if (errorMsg) {
+        alert(errorMsg)
+        return false;
+    }
+};
+
+// summary
+/**
+ *  策略模式利用组合、委托和多态等技术和思想，可以有效地避免多重条件选择语句。
+ *  策略模式提供了对开放—封闭原则的完美支持，将算法封装在独立的 strategy 中，使得它们易于切换，易于理解，易于扩展。
+ *  策略模式中的算法也可以复用在系统的其他地方，从而避免许多重复的复制粘贴工作。
+ *  在策略模式中利用组合和委托来让 Context 拥有执行算法的能力，这也是继承的一种更轻便的替代方案。
+ * 
+ */
+
+/**
+ * 策略模式已经融入到了js语言本身当中，
+ * 我们经常用高阶函数来封装不同的行为，
+ * 并且把它传递到另一个函数中。
+ * 当我们对这些函数发出“调用”的消息时，
+ * 不同的函数会返回不同的执行结果。
+ */
+
+let S = function( salary ) {
+    return salary * 6;
+};
+let A = function( salary ) {
+    return salary * 3;
+};
+// ...
+
+let caculateBonus = function(func, salary) {
+    return func(salary);
+}
+
+caculateBonus(S, 10**5);
+
