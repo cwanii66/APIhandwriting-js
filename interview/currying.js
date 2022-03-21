714255bb4b81add84d560f3d2bd6c60ccb4e3cb9
cwluvani <wh18710455626@126.com>
2022-03-21 10:17:51 +0800

    uncurrying

--- interview/currying.js
+++ interview/currying.js
@@ -10,3 +10,39 @@ const currying = function(func) {
         }
     };
 };
+// cost caculating test
+const cost = (function() {
+    let money = 0;
+
+    return function(...args) {
+        for (let i = 0, l = args.length; i < l; i++) {
+            money += args[i];
+        }
+        return money;
+    }
+})();
+
+const curriedCost = currying(cost); // tansform it to curry function
+
+cost(100);
+cost(200);
+cost(300); // do not sum
+alert(cost()); // input: 600
+
+
+/**
+ * params composing
+ * and return new function to receive next param
+ * then equal to the formal param number ... execute
+ */
+
+// uncurrying
+// in order to generalize
+
+Function.prototype.uncurrying = function() {
+    const self = this;
+
+    return function(...args) {
+        return Reflect.apply(Function.prototype.call, self, args); // 对 call 以 apply的方式调用
+    };
+};
